aefe6e9c00de9341d61dd9cea9506f19739f6d3e
lishiyun19 <lishiyun19@gmail.com>
2016-07-11 14:16:07 +0800

    add Listener & Conf

---
+++ src/main/java/es/source/code/conf/SCOSConf.java
@@ -0,0 +1,11 @@
+package es.source.code.conf;
+
+public class SCOSConf {
+    public static final String LOGIN_SUCCESS = "LoginSuccess";
+
+    public static final String REGISTER_SUCCESS = "RegisterSuccess";
+
+    public static final String SERVER = "http://192.168.1.47:8080/SCOSServer/LoginValidator";
+
+    public static final String SERVICE = "http://192.168.1.47:8080/SCOSServer/FoodUpdateService";
+}
